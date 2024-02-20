34ad13343b1b8eb18dc196f4654860bf2dd8aa1b
gonxa <129299551+gonxa11@users.noreply.github.com>
2024-02-20 09:22:02 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,94 @@
+const displayValorAnterior = document.getElementById('valorAnterior');
+const displayValorActual = document.getElementById('valorActual');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
+
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => {
+        display.agregarNumero(boton.innerHTML);
+    });
+});
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => {
+        display.computar(boton.value);
+    });
+});
+
+class Calculadora {
+    sumar(num1, num2) {
+        return num1 + num2;
+    }
+
+    restar(num1, num2) {
+        return num1 - num2;
+    }
+
+    dividir(num1, num2) {
+        return num1 / num2;
+    }
+
+    multiplicar(num1, num2) {
+        return num1 * num2;
+    }
+
+}
+
+class Display {
+    constructor(displayValorActual, displayValorAnterior) {
+        this.displayValorActual = displayValorActual;
+        this.displayValorAnterior = displayValorAnterior;
+        this.calculador = new Calculadora();
+        this.tipoOperacion = undefined;
+        this.valorActual = '';
+        this.valorAnterior = '';
+        this.signos = {
+            sumar: '+',
+            dividir: '%',
+            multiplicar: '*',
+            restar: '-'
+        }
+    }
+
+    borrar() {
+        this.valorActual = this.valorActual.toString().slice(0, -1);
+        this.imprimirValores();
+    }
+
+    borrarTodo() {
+        this.valorAnterior = '';
+        this.valorActual = '';
+        this.tipoOperacion = undefined;
+        this.imprimirValores();
+    }
+
+    computar(tipo) {
+        this.tipoOperacion !== 'igual' && this.calcular();
+        this.tipoOperacion = tipo;
+        this.valorAnterior = this.valorActual || this.valorAnterior;
+        this.valorActual = '';
+        this.imprimirValores();
+    }
+
+    agregarNumero(numero) {
+        if (numero === '.' && this.valorActual.includes('.')) return
+        this.valorActual = this.valorActual.toString() + numero.toString();
+        this.imprimirValores();
+    }
+
+    imprimirValores() {
+        this.displayValorActual.textContent = this.valorActual;
+        this.displayValorAnterior.textContent = `${this.valorAnterior} ${this.signos[this.tipoOperacion] || ''}`;
+    }
+
+    calcular() {
+        const valorAnterior = parseFloat(this.valorAnterior);
+        const valorActual = parseFloat(this.valorActual);
+
+        if(isNaN(valorActual) || isNaN(valorAnterior)) return
+        this.valorActual = this.calculador[this.tipoOperacion](valorAnterior, valorActual);
+    }
+}
+
+const calculadora = new Calculadora();
+const display = new Display(displayValorActual, displayValorAnterior);
